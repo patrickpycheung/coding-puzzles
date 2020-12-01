021eefa6802568d5f21fc3fdc5c2495f63638ee8
Patrick Cheung <patrick_cheung23@hotmail.com>
2020-12-01 17:09:37 +1100

    Add initial spring boot skeleton.

---
+++ src/main/java/com/test/CodingPuzzlesApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingPuzzlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingPuzzlesApplication.class, args);
+	}
+
+}
